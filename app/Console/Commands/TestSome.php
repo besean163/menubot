926ccb2bd72c209e2dd1e62deaad56a11065b0ff
Сергей Белоусов <besean163@yandex.ru>
2022-12-03 20:07:01 +0400

    make

--- app/Console/Commands/TestSome.php
+++ app/Console/Commands/TestSome.php
@@ -7,32 +7,33 @@ use Illuminate\Console\Command;
 
 class TestSome extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'testsome';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'testsome';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Скрипт для тестирования чего нибудь.';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Скрипт для тестирования чего нибудь.';
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
-    public function handle()
-    {
-        $foodSupplier = FoodSupplier::create([
-            'name' => 'testFoodSupplier',
-            'sourceId' => 'someId',
-        ]);
-        $foodSupplier->save();
-        return Command::SUCCESS;
-    }
+	/**
+	 * Execute the console command.
+	 *
+	 * @return int
+	 */
+	public function handle()
+	{
+		$botToken = '1858930058:AAFRaVAE3XyxFsiREylp9WKP-BXDiuZ5cms';
+		$foodSupplier = FoodSupplier::create([
+			'name' => 'testFoodSupplier',
+			'sourceId' => 'someId',
+		]);
+		$foodSupplier->save();
+		return Command::SUCCESS;
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::any('/', function (Request $request) {
+    Log::info(Request::method());
+    Log::info(Request::url());
+    // return view('welcome');
+    Log::info('in web.');
+    Log::info(Request::all());
+    // return [];
+    $client = new Client();
+    $params =
+        [
+            RequestOptions::HEADERS => [
+                "Content-Type" => "application/json"
+            ],
+            RequestOptions::BODY => json_encode([
+                "chat_id" => "275665865",
+                "text" => "work"
+            ]),
+
+        ];
+    $client->post("https://api.telegram.org/bot1858930058:AAFRaVAE3XyxFsiREylp9WKP-BXDiuZ5cms/sendMessage", $params);
+    return [
+        "chat_id" => "275665865",
+        "text" => "work"
+    ];
+    // exit;
 });
